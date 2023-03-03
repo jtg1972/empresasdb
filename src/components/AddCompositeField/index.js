a0d304fb73d4deefd98532f513ff8214263ee381
jorge toro <jorgetorogutierrez@live.com>
2023-03-02 18:06:34 -0600

    Data input of composite fields almost done

--- src/components/AddCompositeField/index.js
+++ src/components/AddCompositeField/index.js
@@ -61,8 +61,11 @@ updateNumberOperatorsforConcat})=>{
           let sf=[]
           for(let u=0;u<y.length;u++){
             if(u%2==0)
-              if(y[u]=="add text" || y[u]=="concat" || y[u]=="substring")
+              
+              if(y[u]=="add text" || y[u]=="concat")
                 sf.push(y[u+1])
+              if(y[u]=="substring")
+                sf.push(y[u+1].field)
           }
           console.log("sfff",sf)
           updateNumberOperatorsforConcat(y,sf)
@@ -100,9 +103,9 @@ export const AddCompositeField = ({
   const [chars,setChars]=useState(0)
   const numberOperators=["+","-","*","/","substring","add text"]
   const stringOperators=["concat","substring","add text"]
-  const initalOperators=["none","add text","add field","substring"]
+  const initalOperators=["none","add text",/*"add field",*/"substring"]
   console.log("sfadd",stringFields,compositeField)
-
+  let pr=true
   useEffect(()=>{
 
     setField(otmCategoryFields[0]?.name1)
@@ -122,6 +125,12 @@ export const AddCompositeField = ({
   ,[otmCategoryFields])
 
   useEffect(()=>{
+    if(compositeField.length==0)
+      setPrimero(true)
+    else  
+    setPrimero(false)
+  },[compositeField])
+  useEffect(()=>{
     if(primero==false){
     if(stringFields.length==0)
       setOperator(numberOperators[0])
@@ -129,6 +138,8 @@ export const AddCompositeField = ({
       setOperator(stringOperators[0])
     }
   },[stringFields,primero])
+
+  
 
   const searchType=(name)=>{
     let u=otmCategoryFields.filter(
@@ -138,40 +149,141 @@ export const AddCompositeField = ({
       return u[0].type
   }
 
-  const updateNumberOperatorsforConcat=(arr,sf)=>{
+  const updateNumberOperatorsforConcat=(arr,sf,pri=false)=>{
+    let compFields=[]
+    let strFields=[]
+    let uno=false
     if(sf.length>0){
-      console.log("arryy",arr,arr.map((x,index)=>{
-        if(index%2==0)
-        if(numberOperators.filter(y=>(y!=="substring" && y!=="add text" && y!=="none")).includes(x))
+      if(arr.length==2){
+        uno=true
+        if(arr[0]!=="concat" && arr[0]!=="add text" && arr[0]!=="substring"){
+          console.log("entro con dos")
+          let u=searchType(arr[1])
+          if(u=="string"){
+            console.log("entro con dos string")
+
+            strFields=[arr[1]]
+          //if(arr[0]!=="substring")
+            compFields=arr
+          }else{
+            console.log("entro con dos number")
+
+            strFields=[]
+            compFields=["none",arr[1]]
+          }
+          //compFields=["none",arr[1]]
           
-            return "concat"
-        return x
-      }))
-      let scf=arr.map((x,index)=>{
-        if(index%2==0)
-          if(numberOperators.filter(y=>(y!=="substring" && y!=="add text" && y!=="none")).includes(x))
+          console.log("revisa ww",strFields,compFields)
           
-            return "concat"
-        return x
-      })
-      setCompositeField(arr.map((x,index)=>{
-        if(index%2==0)
-          if(numberOperators.filter(y=>(y!=="substring" && y!=="add text" && y!=="none")).includes(x))
+            
+        } 
+        
+        else{
+          let u=searchType(arr[1])
+          if(u=="string"){
+            console.log("entro con dos string")
+
+            strFields=[arr[1]]
+          //if(arr[0]!=="substring")
+            compFields=arr
+          }else{
+            console.log("entro con dos number")
+
+            strFields=[]
+            compFields=["none",arr[1]]
+          }
+          //strFields=[arr[1]]
+          //compFields=arr
+
+          /*let u=searchType(arr[1])
+          if(u=="string"){
+           strFields=[arr[1]]
+            
+          }else{
+            strFields=[]
+            compFields=["none",arr[1]]
+          //strFields=sf
+          compFields=arr*/
+          console.log("revisa wy",strFields,compFields)
+
+        }
+      }else{
+        console.log("revisa 1")
+        compFields=arr.map((x,index)=>{
+          console.log("revisa",x,index)
+          if(index%2==0){
+            if(index!==0){
+              if(numberOperators.filter(y=>(y!=="add text" && y!=="concat" && y!=="substring" && y!=="none")).includes(x))
+            
+                return "concat"
+              
+              return x
+
+            }else{
+              console.log("entro cero")
+              if(x!=="concat" && x!=="add text" && x!=="substring"){
+                console.log("entro none")
+                return "none"
+                  
+              }else 
+                return x
+            }
+          }else
+            return x
           
-            return "concat"
-        return x
-      }))
-      let sf1=[]
-      for(let u=0;u<scf.length;u++){
-        if(u%2==0)
-          if(scf[u]=="add text" || scf[u]=="concat" || scf[u]=="substring")
-            sf1.push(scf[u+1])
+            
+        })
+      
+        /*setCompositeField(arr.map((x,index)=>{
+          if(index%2==0)
+            if(numberOperators.filter(y=>(y!=="substring" && y!=="add text" && y!=="none")).includes(x))
+            
+              return "concat"
+          return x
+        }))*/
       }
-      setStringFields(sf1)
+        
+        
+          let sf1=[]
+          for(let u=0;u<compFields.length;u++){
+            if(u%2==0){
+              if(compFields[u]=="add text" || compFields[u]=="concat" || compFields[u]=="add text" || compFields[u]=="none")
+                sf1.push(compFields[u+1])
+              if(compFields[u]=="substring")
+                sf1.push(compFields[u+1].field)
+            }
+          }
+          if(!uno){
+            setStringFields(sf1)
+            setCompositeField(compFields)
+            setPrimero(false)
+          }else {
+           setStringFields(strFields)
+            setCompositeField(compFields)
+            setPrimero(false)
+          }
+      
     }else{
-      setCompositeField(arr)
-      setStringFields(sf)
-      setPrimero(true)
+      console.log("entro aqui pendejo")
+      if(arr.length==2){
+          console.log("x22")
+          compFields=["none",arr[1]]
+          console.log("revisa ww",strFields,compFields)
+            
+      }else if(arr.length>2) {
+        console.log("x33")
+        compFields=arr
+        compFields[0]="none"
+        console.log("revisa wy",strFields,compFields)
+
+      }else if(arr.length==0)
+        setStringFields([])
+      setCompositeField(compFields)
+     
+      if(arr.length==0)
+        setPrimero(true)
+      else
+        setPrimero(false)
     }
 
   }
@@ -238,33 +350,43 @@ export const AddCompositeField = ({
       }
       <FormButton onClick={()=>{
         let sf=stringFields
-        if(operator!=="add text" && operator!=="substring"){
+        console.log("searchtype",searchType(field),operator)
+
+        if(operator!=="add text" && operator!=="substring" && operator!=="concat"){
           let t=searchType(field)
           if(t=="string"){
             
-            setIsString(true)
+            //setIsString(true)
             sf=[...stringFields,field]
-            setStringFields((e)=>([...e,field]))
+            //setStringFields((e)=>([...e,field]))
           
           }else
             setIsString(false)
           if(primero==true){
-            updateNumberOperatorsforConcat([operator,field],stringFields)
+            updateNumberOperatorsforConcat([operator,field],sf)
             //setCompositeField(e=>([field]))
-            setPrimero(false)
+            //setPrimero(false)
           }else{
             updateNumberOperatorsforConcat([...compositeField,operator,field],sf)
             //setCompositeField(e=>([...e,operator,field]))
           }
+
+        }else if(operator=="concat"){
+          sf=[...stringFields,field]
+          //setStringFields(sf)
+          updateNumberOperatorsforConcat([...compositeField,operator,field],sf)
+        
         }else if(operator=="add text"){
           sf=[...stringFields,addText]
-          setStringFields(sf)
+          //setStringFields(sf)
           updateNumberOperatorsforConcat([...compositeField,operator,addText],sf)
         }else if(operator=="substring"){
           sf=[...stringFields,field]
-          setStringFields(sf)
+          //setStringFields(sf)
           updateNumberOperatorsforConcat([...compositeField,operator,{op:"substring",start,chars,field}],sf)
-        } 
+        } else{
+          updateNumberOperatorsforConcat([...compositeField,operator,field,pr],sf)
+        }
 
       }}>Add</FormButton>
 
